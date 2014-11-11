19e2b20764bfd6b7722a18c4bb0b61ddec9b52a9
asztalosdani <asztalosdani@gmail.com>
2014-11-11 11:28:16 +0100

    UserService fix

--- backend/src/main/java/hu/bme/vik/szoftarch/services/UserService.java
+++ backend/src/main/java/hu/bme/vik/szoftarch/services/UserService.java
@@ -3,16 +3,20 @@ package hu.bme.vik.szoftarch.services;
 import java.util.Objects;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import hu.bme.vik.szoftarch.managers.UserManager;
 
 @Path("/users")
+@Produces("application/json")
+@Consumes("application/json")
 public class UserService {
 
 	@Inject
-	UserManager manager;
+	private UserManager manager;
 
 	@GET
 	public String getUsers() {
