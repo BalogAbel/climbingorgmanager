c86ad6c59077fedde844b8348f2f5945932a1427
asztalosdani <asztalosdani@gmail.com>
2014-11-13 18:53:09 +0100

    login fixed

--- server/rest/src/main/java/hu/bme/vik/szoftarch/climbingorgmanager/rest/services/UserService.java
+++ server/rest/src/main/java/hu/bme/vik/szoftarch/climbingorgmanager/rest/services/UserService.java
@@ -3,7 +3,15 @@ package hu.bme.vik.szoftarch.climbingorgmanager.rest.services;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import hu.bme.vik.szoftarch.climbingorgamanager.backend.exceptions.BadLoginCredentialsException;
@@ -22,10 +30,10 @@ public class UserService {
 	@Inject
 	private UserManager manager;
 
-	@GET
+	@POST
 	@Path("/login")
-	public Response login(@QueryParam("username") String username,
-					   @QueryParam("password") String password) {
+	@Consumes("application/x-www-form-urlencoded")
+	public Response login(@FormParam("username") String username, @FormParam("password") String password) {
 
 		try {
 			Token token = manager.loginRest(username, password);
