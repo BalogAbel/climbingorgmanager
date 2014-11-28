0df8a525f45c93513f0baf66ebac282a9377fd65
asztalosdani <asztalosdani@gmail.com>
2014-11-28 15:09:28 +0100

    EditEquipment, getEquipmentTypes added

--- server/rest/src/main/java/hu/bme/vik/szoftarch/climbingorgmanager/rest/services/EquipmentService.java
+++ server/rest/src/main/java/hu/bme/vik/szoftarch/climbingorgmanager/rest/services/EquipmentService.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.Response;
 import hu.bme.vik.szoftarch.climbingorgamanager.backend.exceptions.NoSuchEquipmentException;
 import hu.bme.vik.szoftarch.climbingorgamanager.backend.managers.EquipmentManager;
 import hu.bme.vik.szoftarch.climbingorgmanager.core.entities.Equipment;
+import hu.bme.vik.szoftarch.climbingorgmanager.core.entities.EquipmentType;
 import hu.bme.vik.szoftarch.climbingorgmanager.core.entities.Rental;
 
 @Path("/equipments")
@@ -49,6 +51,12 @@ public class EquipmentService {
 		return Response.status(Response.Status.OK).build();
 	}
 
+	@PUT
+	public Response editEquipment(Equipment equipment) {
+		equipmentManager.editEquipment(equipment);
+		return Response.status(Response.Status.OK).build();
+	}
+
 	@DELETE
 	@Path("/{id}")
 	public Response removeEquipment(@PathParam("id") long id) {
@@ -59,7 +67,12 @@ public class EquipmentService {
 		} catch (NoSuchEquipmentException e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity("No equipment found for id.").build();
 		}
-		return Response.status(Response.Status.OK).entity(equipment)
-				.build(); //TODO doesn't have to return entity, remove when done
+		return Response.status(Response.Status.OK).build();
+	}
+
+	@GET
+	@Path("/types")
+	public List<EquipmentType> getEquipmentTypes() {
+		return equipmentManager.getEquipmentTypes();
 	}
 }
